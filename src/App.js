815f8f191ca4b2337741175d397e60f4667e2a53
danie <danielcardona0110@outlook.com>
2023-10-14 12:36:07 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import UseRefUseCase1 from "./Components/UseRefUseCase1";
-import UseRefUseCase2 from "./Components/UseRefUseCase2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Learn React</h1>
-        <UseRefUseCase1 />
-        <UseRefUseCase2 />
       </header>
     </div>
   );
